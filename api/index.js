d5d338132252589bad107535c3330db2c4e030c1
DesiredName <wc3-enter@yandex.ru>
2024-10-13 16:40:35 +0200

    'update'

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const path = require('path');
+const handlebars = require("express-handlebars");
 
-// Create application/x-www-form-urlencoded parser
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const customHandlebars = handlebars.create({ layoutsDir: "./views" });
 const port = process.env.PORT || 3000;
+
+app.engine("handlebars", customHandlebars.engine);
+app.set("view engine", "handlebars");
 
 app.use("/files", express.static("public"));
 
@@ -17,16 +19,6 @@ app.get("/home" , (req , res)=>{
 
 app.get('/uploadUser', function (req, res) {
 	res.sendFile(path.join(__dirname, '..', 'components', 'user_upload_form.htm'));
-});
-
-app.post('/uploadSuccessful', urlencodedParser, async (req, res) => {
-	try {
-		await sql`INSERT INTO Users (Id, Name, Email) VALUES (${req.body.user_id}, ${req.body.name}, ${req.body.email});`;
-		res.status(200).send('<h1>User added successfully</h1>');
-	} catch (error) {
-		console.error(error);
-		res.status(500).send('Error adding user');
-	}
 });
 
 app.listen(port, () => console.log(`Server ready on port ${port}`));
