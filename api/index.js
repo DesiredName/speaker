9256b60ed3189879d16622a64237f2a6116ea4a9
DesiredName <wc3-enter@yandex.ru>
2024-10-13 17:10:17 +0200

    'update'

--- api/index.js
+++ api/index.js
@@ -2,30 +2,20 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const handlebars = require("express-handlebars");
-const {expressCspHeader: csp, NONE, UNSAFE_EVAL} = require('express-csp-header');
+const path = require('path');
 
 const customHandlebars = handlebars.create({ layoutsDir: "./views" });
 const port = process.env.PORT || 3000;
 
 app.engine("handlebars", customHandlebars.engine);
 app.set("view engine", "handlebars");
-
-app.use((err, req, res, next) => {
-    res.status(500).send('Something broke!')
-})
-app.use(csp({
-    policies: {
-        'default-src': [UNSAFE_EVAL],
-        'script-src': [UNSAFE_EVAL],
-    }
-}))
+app.set('views', path.join(__dirname, 'views'));
 app.use("/files", express.static("public"));
-app.get("/", (req, res) => res.send("Express on Vercel"));
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
 app.get("/check" , (req , res)=>{
     res.json({ port });
 });
-
 app.get("/home" , (req , res)=>{
     try {
         return res.render("index", { port });
@@ -33,7 +23,6 @@ app.get("/home" , (req , res)=>{
         return ex.toString();
     }
 });
-
 app.get('/uploadUser', function (req, res) {
 	res.sendFile(path.join(__dirname, '..', 'components', 'user_upload_form.htm'));
 });
