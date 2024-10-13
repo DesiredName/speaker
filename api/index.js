53b99ba9af833ca4c02cf5d58131c5ffffc05e31
DesiredName <wc3-enter@yandex.ru>
2024-10-13 14:32:26 +0200

    'update'

--- api/index.js
+++ api/index.js
@@ -1,18 +1,11 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => res.send('Home Page Route'));
+app.get("/", (req, res) => res.send(`Express on Vercel ${process.env.VERCEL_URL}`));
 
-app.get('/about', (req, res) => res.send('About Page Route'));
+app.listen(3100, () => console.log("Server ready on port 3100."));
 
-app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
-
-app.get('/contact', (req, res) => res.send('Contact Page Route'));
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+module.exports = app;
 
 
 // const express = require("express");
